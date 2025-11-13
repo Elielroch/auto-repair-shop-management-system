e7c77e12634ba5454eb139b6a19983ca306af246
Eliel Rocha <elielrocha382@gmail.com>
2025-11-13 17:59:16 -0300

    port

--- object-recognition/src/main.py
+++ object-recognition/src/main.py
@@ -40,5 +40,9 @@ def serve(path):
             return "index.html not found", 404
 
 
+# ... (código anterior)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port, debug=True)
